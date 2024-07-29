3ed20f630aa5c02a9ee6a4bb7469eae54be7b409
Denilson Uariua <denuariua@gmail.com>
2024-07-29 22:21:37 +0200

    Fixes head eyes not showing

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -22,7 +22,14 @@ namespace Snake
             {GridValue.Empty, Images.Empty },
             {GridValue.Snake, Images.Body },
             {GridValue.Food, Images.Food }
+        };
 
+        private readonly Dictionary<Direction, int> dirToRotation = new()
+        {
+            {Direction.Up, 0},
+            {Direction.Right, 90 },
+            {Direction.Down, 180 },
+            {Direction.Left, 270 }
         };
 
         private readonly int rows = 15, cols = 15;
@@ -85,7 +92,7 @@ namespace Snake
         }
         private async Task GameLoop()
         {
-            while (true)
+            while (!gameState.GameOver)
             {
                 await Task.Delay(100);
                 gameState.Move();
@@ -97,6 +104,7 @@ namespace Snake
             Image[,] images = new Image[rows, cols];
             GameGrid.Rows = rows;
             GameGrid.Columns = cols;
+            GameGrid.Width = GameGrid.Height * (cols/ (double)rows);
 
             for (int r = 0; r < rows; r++)
             {
@@ -104,7 +112,8 @@ namespace Snake
                 {
                     Image image = new Image
                     {
-                        Source = Images.Empty
+                        Source = Images.Empty,
+                        RenderTransformOrigin = new Point(0.5, 0.5)
                     };
 
                     images[r, c] = image;
@@ -118,8 +127,10 @@ namespace Snake
         {
 
             DrawGrid();
+            DrawSnakeHead();
             ScoreText.Text = $"Score {gameState.Score}";
         }
+
         private void DrawGrid()
         {
             for (int r = 0; r < rows; r++)
@@ -128,10 +139,32 @@ namespace Snake
                 {
                     GridValue gridVal = gameState.Grid[r, c];
                     gridImages[r, c].Source = gridValToImage[gridVal];
+                    gridImages[r, c].RenderTransform = Transform.Identity;
                 }
             }
         }
+        private void DrawSnakeHead()
+        {
+            Position headPos = gameState.HeadPosition();
+            Image image = gridImages[headPos.Row, headPos.Column];
+            image.Source = Images.Head;
 
+            int rotation = dirToRotation[gameState.Dir];
+            image.RenderTransform = new RotateTransform(rotation);
+        }
+        private async Task DrawDeadSnake()
+        {
+            List<Position> positions = new List<Position>(gameState.SnakePosition());
+
+            for(int i = 0; i < positions.Count; i++)
+            {
+                Position pos = positions[i];
+                ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
+
+                gridImages[pos.Row, pos.Column].Source = source;
+                await Task.Delay(50);
+            }
+        }
         private async Task ShowCountDown()
         {
             for (int i = 3; i >= 1; i--)
@@ -143,9 +176,11 @@ namespace Snake
 
         private async Task ShowGameOver()
         {
+            await DrawDeadSnake();
             await Task.Delay(1000);
             Overlay.Visibility = Visibility.Visible;
             OverlayText.Text = "PRESS ANY KEY TO START";
+
         }
     }
 }
